815f45396a13c15ed0bcea4816de8125982a8ab4
Elan Zinkow <ezinkow@gmail.com>
2020-12-17 22:32:48 -0600

    heroku push

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var PORT = process.env.PORT || 3000;
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -8,8 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use(favicon(__dirname + '/public/favicon.png'))
-
-const PORT = process.env.PORT || 3000;
 
 //connect server with routes files
 require("./routes/apiRoutes")(app);
