806e568ffb81947d30c38bc50c2974e630fac205
Elan Zinkow <ezinkow@gmail.com>
2020-12-17 22:16:42 -0600

    heroku add again

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const favicon = require('express-favicon')
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use(favicon(__dirname + '/public/favicon.png'))
